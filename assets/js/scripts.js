e09445c08a81e31edd3d8c93523e60177533932a
ingridev-cf <ingrid.melo27@gmail.com>
2025-07-28 13:10:00 -0300

    feat: about and contact

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -13,3 +13,12 @@ function changeTheme() {
 }
 
 toggleTheme.addEventListener("click", changeTheme);
+
+const menuLinks = document.querySelectorAll(".menu__link");
+
+menuLinks.forEach(item => {
+    item.addEventListener("click", () => {
+        menuLinks.forEach(i => i.classList.remove("active"));
+        item.classList.add("active");
+    })
+})
